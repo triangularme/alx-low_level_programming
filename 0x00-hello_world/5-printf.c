c3b33893c70e0b3e5c90d9766f97a8f51e7ae732
root <triangularme@gmail.com>
2023-03-02 16:42:01 -0800

    file 5 and 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
-int main()
+/**
+* main - is the entry for the program
+* Description: printf
+* Return: zero if no error.
+*/
+
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
 }
