72900846b9b4c22891b35acd3830234205c30c7b
root <triangularme@gmail.com>
2023-03-06 05:27:10 -0800

    some new files

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * Main - entry point
+ *
+ * Desription: print digits from 0 - 9
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+
+	putchar("\n");
+
+	return (0);
+}
