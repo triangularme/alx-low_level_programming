c728a9b98d4dc3fc64cd507761d427a61325c291
root <triangularme@gmail.com>
2023-03-06 12:29:15 -0800

    fix 4&5

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,8 +14,8 @@ int main(void)
 
 	while (ch <= 'z')
 	{
-		if (ch != 'e' || ch != 'q')
-		putchar(ch);
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
 		ch++;
 	}
 	putchar('\n');
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -16,7 +16,7 @@ int main(void)
 	for (a = 0; a < 10; a++)
 		printf("%d", a);
 
-	putchar("\n");
+	printf("\n");
 
 	return (0);
 }
